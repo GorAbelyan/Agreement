52e8ba86bc0e24caef060dec1239bc07c91892ed
GorAbelyan <gor.abelyan.97@gmail.com>
2022-07-20 03:47:00 +0400

    fix calendar bag

--- AgreementManagement/wwwroot/js/datapicker.js
+++ AgreementManagement/wwwroot/js/datapicker.js
@@ -5,19 +5,4 @@ $(document).ready(function () {
     console.log("sdfdsf");
 });
 
-$('.datepicker').datepicker({
-    format: 'mm/dd/yyyy',
-    changemonth: true,
-    changeyear: true,
-    startDate: '-3d'
-}).on('changeDate', function (ev) {
-    if ($('#datepicker').valid()) {
-        $('#datepicker').removeClass('invalid').addClass('success');
-    }
-});;
-
-$('.datepicker').on('focus', function (e)
-{
-    e.preventDefault();
-    $(this).attr("autocomplete", "off");
-});
+ 
